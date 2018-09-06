55e8653f31c785972e44efad4482f1ccffd47c35
Mauricio Aniche <mauricioaniche@gmail.com>
2018-09-06 17:22:06 +0200

    missing getting started code

--- src/test/java/tudelft/gettingstarted/GettingStartedTest.java
+++ src/test/java/tudelft/gettingstarted/GettingStartedTest.java
@@ -20,6 +20,6 @@ public class GettingStartedTest {
     @Test
     public void addFiveToMinus20() {
         int result = new GettingStarted().addFive(-20);
-        Assertions.assertEquals(15,result);
+        Assertions.assertEquals(-15,result);
     }
 }
