0b2ffa1c5b225b57eb809bcdefe3e930b5d908bb
Mauricio Aniche <mauricioaniche@gmail.com>
2018-09-06 17:26:21 +0200

    making it as an exercise

--- src/test/java/tudelft/gettingstarted/GettingStartedTest.java
+++ src/test/java/tudelft/gettingstarted/GettingStartedTest.java
@@ -6,20 +6,22 @@ import org.junit.jupiter.api.Test;
 public class GettingStartedTest {
 
     @Test
-    public void addFiveToZero() {
-        int result = new GettingStarted().addFive(0);
-        Assertions.assertEquals(5, result);
-    }
-
-    @Test
     public void addFiveTo20() {
         int result = new GettingStarted().addFive(20);
         Assertions.assertEquals(25,result);
     }
 
-    @Test
-    public void addFiveToMinus20() {
-        int result = new GettingStarted().addFive(-20);
-        Assertions.assertEquals(-15,result);
-    }
+//    UNCOMMENT THE CODE BELOW, AND FILL THE GAPS!
+
+//    @Test
+//    public void addFiveToZero() {
+//        int result = new GettingStarted().addFive(???);
+//        Assertions.assertEquals(???, result);
+//    }
+//
+//    @Test
+//    public void addFiveToMinus20() {
+//        int result = new GettingStarted().addFive(???);
+//        Assertions.assertEquals(????,result);
+//    }
 }
